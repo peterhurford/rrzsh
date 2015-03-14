cea97c388ab0b1f0d9f8934fa75cafa4034eede4
Peter Hurford <peter@peterhurford.com>
2015-03-14 13:41:57 -0500

    Initial plugin

---
+++ README.md
@@ -0,0 +1,13 @@
+## rrzsh - A pirate's favorite plugin.
+
+A plugin for R commands from your terminal.
+
+* `rr` - opens your R console.
+
+* `rr document` - documents the R package that you are in.  `document <path/to/package>` documents that package (e.g., `document ~/dev/package`).
+
+* `rr test` - runs the dests for the package you are in.  `test <path/to/package>` tests that package (e.g., `test ~/dev/package`).
+
+* `rr send <commit message>` - Like [send.zsh](https://github.com/robertzk/send.zsh), but hacked to include R.  If your repo is not a package, it will do `git add .`, `git commit -a -m <commit message>` and `git push origin <the branch you are on>`.  If you are in an R package directory, before doing that, it will document your code and run your tests.
+
+* `rr <command>` - runs that command in R.  `r "mean(2, 4, 5)"` outputs 2.
---
+++ rzsh.plugin.zsh
@@ -0,0 +1,57 @@
+rr() {
+  if [ $# -eq 0 ]; then; R
+  elif [ $1 = "document" ]; then rr_document $@
+  elif [ $1 = "test" ]; then rr_test $@
+  elif [ $1 = "send" ]; then rr_send $@
+  else; Rscript -e $@
+  fi
+}
+
+rr_document() {
+  shift
+  if [ $# -eq 0 ]; then; Rscript -e "devtools::document()";
+  else; Rscript -e "devtools::document($1)"
+  fi
+}
+
+rr_test() {
+  shift
+  if [ $# -eq 0 ]; then; Rscript -e "library(devtools); test()";
+  else; Rscript -e "library(devtools); test($1)"
+  fi
+}
+
+rr_pull_or_push() {
+  if [ $# -eq 2 ]; then
+    git $1 -q $2 `git rev-parse --abbrev-ref HEAD`
+  else
+    git $1 -q origin `git rev-parse --abbrev-ref HEAD`
+  fi
+}
+
+rr_pull() {
+  rr_pull_or_push "pull" $@
+}
+
+rr_push() {
+  rr_pull_or_push "push" $@ &
+}
+
+rr_send() {
+  shift
+  print "Documenting..."
+  rr_document 'shift'
+  print "Testing..."
+  rr_test 'shift'
+  print "Committing..."
+  git add "$(git rev-parse --show-toplevel)"
+  if [ $# -eq 1 ]; then
+   git commit -a -m "$1"
+  else
+   git commit -a -m "I'm too lazy to write a commit message."
+  fi
+  print "Pulling..."
+  rr_pull
+  print "Pushing..."
+  rr_push
+}
